e42c3580a23b209112ec718314282e27743c2dc1
Sergey Kiselyov <sk171299@gmail.com>
2025-10-08 15:19:07 +0400

    Increased speed of radius search

--- app/src/main/java/com/kamikadze328/memo/repository/MemoDao.kt
+++ app/src/main/java/com/kamikadze328/memo/repository/MemoDao.kt
@@ -42,4 +42,38 @@ internal interface MemoDao {
      */
     @Query("SELECT * FROM memo WHERE id = :memoId")
     fun getMemoById(memoId: Long): Memo
+
+    /**
+     *
+     * @param lat The latitude of the location.
+     * @param lon The longitude of the location.
+     * @param latCos The cosine of the latitude.
+     * @param degPerLat The degrees per latitude.
+     * @param degPerLon The degrees per longitude.
+     * @param radiusMeters The radius of search in meters.
+     *
+     * @return all memos that are currently in the database and have not yet been marked as "done".
+     *
+     */
+    @Query(
+        """
+                SELECT *
+                FROM memo
+                WHERE isDone = 0
+                  AND reminderLatitude BETWEEN (:lat - :degPerLat) AND (:lat + :degPerLat)
+                  AND reminderLongitude BETWEEN (:lon - :degPerLon) AND (:lon + :degPerLon)
+                  AND (
+                      ((reminderLatitude - :lat) * 111320.0) * ((reminderLatitude - :lat) * 111320.0) +
+                      ((reminderLongitude - :lon) * 111320.0 * :latCos) * ((reminderLongitude - :lon) * 111320.0 * :latCos)
+                  ) <= (:radiusMeters * :radiusMeters)
+        """
+    )
+    fun findNearOpenedMemoByFlatDistance(
+        lat: Double,
+        lon: Double,
+        latCos: Double,
+        degPerLat: Double,
+        degPerLon: Double,
+        radiusMeters: Double
+    ): List<Memo>
 }
--- app/src/main/java/com/kamikadze328/memo/repository/Repository.kt
+++ app/src/main/java/com/kamikadze328/memo/repository/Repository.kt
@@ -1,8 +1,8 @@
 package com.kamikadze328.memo.repository
 
-import androidx.room.Room
 import android.content.Context
 import androidx.annotation.WorkerThread
+import androidx.room.Room
 import com.kamikadze328.memo.model.Memo
 import com.kamikadze328.memo.repository.migrations.MIGRATION_1_2
 import kotlinx.coroutines.flow.Flow
@@ -41,4 +41,24 @@ internal object Repository : IMemoRepository {
 
     @WorkerThread
     override fun getMemoById(id: Long): Memo = database.getMemoDao().getMemoById(id)
+
+    @WorkerThread
+    override suspend fun findNearMemoByFlatDistance(
+        latitude: Double,
+        longitude: Double,
+        radiusMeters: Double
+    ): List<Memo> {
+        val latCos = kotlin.math.cos(Math.toRadians(latitude))
+        val degPerLat = radiusMeters / 111_320.0
+        val degPerLon = radiusMeters / (111_320.0 * latCos)
+
+        return database.getMemoDao().findNearOpenedMemoByFlatDistance(
+            lat = latitude,
+            lon = longitude,
+            latCos = latCos,
+            degPerLat = degPerLat,
+            degPerLon = degPerLon,
+            radiusMeters = radiusMeters
+        )
+    }
 }
